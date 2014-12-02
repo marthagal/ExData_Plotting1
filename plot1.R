3be9251a83988d5671940393f28578a0bca4d845
marthagal <madickinson@gmail.com>
2014-12-02 14:20:59 -0600

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,27 @@
+library(lubridate)
+
+power <- read.table("household_power_consumption.txt", sep=";", header=TRUE)
+
+power$DateSub <- as.character(power$Date)
+datePower <- subset(power, DateSub == "1/2/2007" | DateSub == "2/2/2007")
+datePower$DateSub <- NULL
+
+datePower$Dates <- dmy(datePower$Date)
+datePower$DateTime <-  paste(as.character(datePower$Date),
+                             as.character(datePower$Time))
+datePower$DateTimes <- dmy_hms(datePower$DateTime)
+datePower$DateTime <- NULL
+datePower$Date <- NULL
+datePower$Time <- NULL
+
+datePower$Global_active_power <- as.numeric(as.character(datePower$Global_active_power))
+datePower$Global_reactive_power <- as.numeric(as.character(datePower$Global_reactive_power))
+datePower$Voltage <- as.numeric(as.character(datePower$Voltage))
+datePower$Global_intensity <- as.numeric(as.character(datePower$Global_intensity))
+datePower$Sub_metering_1 <- as.numeric(as.character(datePower$Sub_metering_1))
+datePower$Sub_metering_2 <- as.numeric(as.character(datePower$Sub_metering_2))
+
+#Plot 1
+png("plot1.png", width=480, height=480)
+hist(datePower$Global_active_power, xlab = "Global Active Power (kilowatts)", col = "red", main = "Global Active Power")
+dev.off()
